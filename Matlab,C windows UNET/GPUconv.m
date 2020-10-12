703fe445e96a2e8cd0909d74ffc21099bd10f549
labis7 <labis_skoufis@hotmail.com>
2020-10-12 15:46:35 +0300

    Benchmarks Completed!

--- Matlab,C windows UNET/GPUconv.m
+++ Matlab,C windows UNET/GPUconv.m
@@ -1,6 +1,32 @@
+% %clear all
+% ch=256;
+% f_num=128;
+% dim=32;
+% 
+% tic
+% %gpuDevice(1);
+% img = single(rand(dim,dim,ch,'gpuArray'));
+% 
+% f = single(rand(3,3,ch,f_num,'gpuArray'));
+% 
+% bias = single(rand(f_num,1,'gpuArray'));
+% 
+% imgdl =dlarray(img,'SSC');
+% 
+% dlY = dlconv(imgdl,f,bias,'padding','same');
+% 
+% %rescpu = gather(res);
+% rescpu = gather(dlY);
+% wait(gpuDevice)
+% toc
+%size(dlY)
+%clear all
+%gpuDevice(1);
+%clearvars -global
 %%% CPU
-clear all
-clearvars -global
+
+%clear all
+%clearvars -global
 ch=1;
 dim=128;
 f_num=16;
@@ -23,6 +49,8 @@ for i = 1:5
         skips{i} = res; %skip neuron
         res = maxpool(res,2,'Stride',2);
         dim=dim/2;
+        res = gather(res);
+        wait(gpuDevice);
         el=toc(st);
         sprintf("Max-pool %d : %0.2f ms",i,el*1000)
     end
@@ -38,11 +66,16 @@ for i = 6:9
     %transposed conv
     f_num=f_num/2;%128,64,32,16
 
-    f_t =single(rand(2,2,f_num,ch));
-    b_t = single(rand(f_num,1));
+    f_t =single(rand(2,2,f_num,ch, 'gpuArray'));
+    b_t = single(rand(f_num,1, 'gpuArray'));
 
     tconv = dltranspconv(res,f_t,b_t,'Stride',2);
 
+    
+    tconv = gather(tconv);
+    wait(gpuDevice);
+    
+    
     dim = dim*2;
     el=toc(st);
     sprintf("TConv %d : %0.2f ms",i,el*1000)
@@ -59,11 +92,15 @@ end
 
 %%%%%%%%%% Final 1x1 Convolution %%%%%%%%%%%%
 f_num=1;
-f = single(rand(1,1,ch,f_num));
+f = single(rand(1,1,ch,f_num, 'gpuArray'));
 
-bias = single(rand(f_num,1));
+bias = single(rand(f_num,1, 'gpuArray'));
 
 out = dlconv(res,f,bias, 'padding','same');
+
+out = gather(out);
+wait(gpuDevice);
+
 elapsed=toc(start);
 fprintf("Overall Time : %.2f ms",elapsed*1000);
 
@@ -80,18 +117,22 @@ function res = conv_block(f_num,ch,dim,i)
     st=tic;
 
     %CONV1
-    img = single(rand(dim,dim,ch));
+    img = single(rand(dim,dim,ch, 'gpuArray'));
 
-    f = single(rand(3,3,ch,f_num));
+    f = single(rand(3,3,ch,f_num, 'gpuArray'));
 
-    bias = single(rand(f_num,1));
+    bias = single(rand(f_num,1, 'gpuArray'));
 
     imgdl =dlarray(img,'SSC');
 
     conv1_1 = dlconv(imgdl,f,bias, 'padding','same');
     
+    
     %ReLu
     conv1_1 = relu(conv1_1);
+    
+    conv1_1 = gather(conv1_1);
+    wait(gpuDevice);
     
     el=toc(st);
     sprintf("Conv %d.1 : %0.2f ms",i,el*1000)
@@ -99,16 +140,19 @@ function res = conv_block(f_num,ch,dim,i)
     %CONV2--------------------------------------------------
     
     ch=f_num;
-    img = single(rand(dim,dim,ch));
+    img = single(rand(dim,dim,ch, 'gpuArray'));
 
-    f = single(rand(3,3,ch,f_num));
+    f = single(rand(3,3,ch,f_num, 'gpuArray'));
 
-    bias = single(rand(f_num,1));
+    bias = single(rand(f_num,1, 'gpuArray'));
     conv1_2 = dlconv(conv1_1,f,bias, 'padding','same');
     
     %ReLu
     res = relu(conv1_2);
 
+    res = gather(res);
+    wait(gpuDevice);
+    
     el=toc(st);
     sprintf("Conv %d.2 : %0.2f ms",i,el*1000)
 end
--- ReadMe.txt
+++ ReadMe.txt
@@ -3,5 +3,6 @@ Baseline's files: basic testing files, + Completed stream communication and mult
 Basic Convolution Calcs: Includes the simple not optimized algorithms
 Completed: Includes the latest versions of Accelerators(written in HLS)
 Vitis_files: Inludes the latest FPGA implementations of the accelerators
+Matlab,C Windows Unet: Indcludes the UNET architecture, able to run on Windows(C) as a dummy neural network and the matlab dummy for benchmarking purposes. 
 
 More information about Neural Network and functions read Higher level language implementations + Thesis by Labis Skoufis.
